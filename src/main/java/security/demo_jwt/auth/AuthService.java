f149d023bdbf8aaf2f9f72c805f17ecf0aff6a44
Alejo Carobolante <alejocarobolante@gmail.com>
2025-11-28 01:00:08 -0300

    Login implementado

--- src/main/java/security/demo_jwt/auth/AuthService.java
+++ src/main/java/security/demo_jwt/auth/AuthService.java
@@ -1,5 +1,8 @@
 package security.demo_jwt.auth;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,9 +24,16 @@ class AuthService {
     private final RoleRepository roleRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     public AuthResponse login(LoginRequest request) {
-        return null;
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        UserDetails user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
